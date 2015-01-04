c4787e6d7df62ff65c36e6e254ad9c7427832e03
nulltoken <emeric.fermas@gmail.com>
2015-01-04 22:46:45 +0100

    Use Mono 3.6 on Travis CI Linux platform

--- CI/travis.linux.install.deps.sh
+++ CI/travis.linux.install.deps.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -ev
 
+MONO_VER=3.6.0
+
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/tpokorra:/mono/xUbuntu_12.04/ /' >> /etc/apt/sources.list.d/mono-opt.list"
 
 curl http://download.opensuse.org/repositories/home:/tpokorra:/mono/xUbuntu_12.04/Release.key | sudo apt-key add -
 
 sudo apt-get update
-sudo apt-get install mono-opt cmake
+sudo apt-get install mono-opt=${MONO_VER} cmake
